4a6b7c2c0c3c3d10641273394a3d69fe48f3d62b
Shubham Kashyap <shubhamkashyap107@gmail.com>
2026-01-08 16:57:32 +0530

    Commit

---
+++ Demo/app.js
@@ -0,0 +1,16 @@
+//console.log("Hello World")
+//console.log("Hello how are you?")
+
+
+// variables
+
+// var name = "Shubham";
+// var age = 22;
+// var isPresent = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(isPresent);
+
+var data;
+data = 'xyz'
