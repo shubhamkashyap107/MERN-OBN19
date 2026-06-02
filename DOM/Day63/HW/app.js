7a7f42c0d2848c5ced69251d536c44ba51f22ca6
Shubham <shubhamkashyap107@gmail.com>
2026-06-02 16:03:45 +0530

    Commit

---
+++ DOM/Day63/HW/app.js
@@ -0,0 +1,76 @@
+const input = document.getElementById("ip")
+const button = document.getElementById("btn")
+const pending = document.getElementById("pending-cards-container")
+const allContainers = document.getElementsByClassName("abc")
+
+
+// let count = 0
+
+
+button.addEventListener("click", () => {
+    const val = input.value
+
+    if(!val)
+    {
+        alert("Please enter some text")
+        return
+    }
+
+    const card = document.createElement("article")
+    // card.classList.add()
+    card.setAttribute("class", "my-card")
+    card.setAttribute("draggable", true)
+    card.setAttribute("id", Date.now())
+    // card.style.border = ""
+    // card.setAttribute("id", Math.random())
+    // card.setAttribute("id", count)
+    // count++
+    card.innerText = val
+    pending.append(card)
+    input.value = ""
+
+})
+
+
+for(let item of allContainers)
+{
+    item.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("kyekanaam", e.target.id)
+    })
+
+    item.addEventListener("dragover", (e) => {
+        e.preventDefault()
+    })
+
+    item.addEventListener("drop", (e) => {
+        e.preventDefault()
+
+        const id = e.dataTransfer.getData("kyekanaam")
+        const card = document.getElementById(id)
+
+        if(e.target.id == "pending-cards-container")
+        {
+            card.style.border = "2px solid red"
+        }
+        else if(e.target.id == "progress-cards-container")
+        {
+            card.style.border = "2px solid yellow"
+        }
+        else{
+            card.style.border = "2px solid green"
+        }
+
+        e.target.append(card)
+
+    })
+}
+
+
+
+
+window.addEventListener("keydown", (e) => {
+    if(e.key == "Enter")
+    {
+        button.click()
+    }
+})
